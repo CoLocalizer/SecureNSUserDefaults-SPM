7bfda3c62c1cc89534994036c59c34f6d24f932d
Viktor Yevtushenko <viktor.yevtushenko@techwings.com>
2025-04-03 15:18:39 +0200

    Replaced structure

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "SecureNSUserDefaults",
     platforms: [
         .macOS(.v10_13),
-        .iOS(.v11)
+        .iOS(.v11),
     ],
     products: [
         .library(
@@ -13,9 +13,15 @@ let package = Package(
             targets: ["SecureNSUserDefaults"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/kelp404/CocoaSecurity.git", from: "1.2.1")
+    ],
     targets: [
         .target(
             name: "SecureNSUserDefaults",
+            dependencies: [
+                .product(name: "CocoaSecurity", package: "CocoaSecurity")
+            ],
             path: "Sources/SecureNSUserDefaults",
             publicHeadersPath: "."
         )
